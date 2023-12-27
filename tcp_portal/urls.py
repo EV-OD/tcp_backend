951b03af0ad2a1e5cfaa4094eb47086ad1355531
EV-OD <evod599@gmail.com>
2023-12-27 16:17:37 +0545

    added register page

--- tcp_portal/urls.py
+++ tcp_portal/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.loginPage, name='login'),
+    path('register/', views.registerPage, name='register'),
+
 ]
--- tcp_portal/views.py
+++ tcp_portal/views.py
@@ -8,3 +8,7 @@ def index(request):
 def loginPage(request):
   template = loader.get_template('login.html')
   return HttpResponse(template.render())
+
+def registerPage(request):
+  template = loader.get_template('register.html')
+  return HttpResponse(template.render())
